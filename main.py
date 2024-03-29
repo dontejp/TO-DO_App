d18d079bcb5d683c03e4331dd899f61e51e329f8
DonteJP <dontejp@gmail.com>
2024-03-29 16:29:15 -0400

    Very basic CLI To-Do app

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello World')
+user_prompt = "Enter a task:"
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todo_list = [todo1, todo2, todo3]
+print(todo_list)
