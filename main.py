bae81d10d2c3061d540b1c0ade9f99cdab3d9284
DonteJP <dontejp@gmail.com>
2024-04-03 13:43:34 -0400

    Added Match-Case functionality

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-user_prompt = "Enter a task:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
+todos = []
 
-todo_list = [todo1, todo2, todo3]
-print(todo_list)
+while True:
+    user_action = input("Type add , show or exit: ")
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show':
+            print(todos)
+        case 'exit':
+            break
+print ("Program exited!")
