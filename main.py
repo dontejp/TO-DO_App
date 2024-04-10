e2849444bc32c44f63b907790bb922ee363750da
DonteJP <dontejp@gmail.com>
2024-04-10 15:58:53 -0400

    Added Edit feature with a numbering system on List

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add , show or exit: ")
+    user_action = input("Type add , show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -9,8 +9,16 @@ while True:
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
+            i = 1
             for task in todos:
-                print(task)
+                print( str(i) + " " + task)
+                i = i + 1
+        case 'edit':
+            number = int(input("Enter the number of the todo item you want to edit: "))
+            number = number - 1
+            new_todo = input("Enter a new todo: ")
+            todos[number] = new_todo
         case 'exit':
             break
+
 print ("Program exited!")
