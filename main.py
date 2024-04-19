ca231618ea066970cb5d9a25cb7f3bc7c6f00ab6
DonteJP <dontejp@gmail.com>
2024-04-18 21:07:02 -0400

    added complete task feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add , show, edit or exit: ")
+    user_action = input("Type add , complete, show, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,12 +10,16 @@ while True:
             todos.append(todo)
         case 'show':
             for index, task in enumerate(todos):
-                print( index, '-', task)
+                print( f"{index + 1}-{task}")
         case 'edit':
             number = int(input("Enter the number of the todo item you want to edit: "))
             number = number - 1
             new_todo = input("Enter a new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Enter the number of the taskw you'd like to remove: "))
+            todos.pop(number-1)
+
         case 'exit':
             break
 
